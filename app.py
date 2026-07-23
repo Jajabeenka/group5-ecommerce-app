ab8f7f2307f086f5b19b0041827f2eba6e68c50a
jessimey <mae.delapaz2003@gmail.com>
2026-07-23 11:41:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def payment_success():
 
 # 3. Run the development server
 if __name__ == "__main__":
-   socketio.run(app,host='0.0.0.0',port=5000,debug=True)
+   socketio.run(app,host='0.0.0.0',port=80,debug=True)
