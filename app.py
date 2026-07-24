1b70332bde2d8b5f4ae2e3f12b448be718f2705d
Yukin <131948029+yukarikinmo8@users.noreply.github.com>
2026-07-24 10:55:07 +0800

    chore: fix flask.app to allow run on prod

--- app.py
+++ app.py
@@ -144,4 +144,9 @@ def login():
 
 # 3. Run the development server
 if __name__ == "__main__":
-   socketio.run(app,host='0.0.0.0',port=5000,debug=True)
+   socketio.run(
+        app,
+        host="0.0.0.0",
+        port=5000,
+        allow_unsafe_werkzeug=True
+    )
